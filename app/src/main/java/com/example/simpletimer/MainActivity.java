f37b03da0f66d5c1a3afabd6c9797d55a5636388
rodoggx <mikerodolph@yahoo.com>
2016-08-11 16:33:16 -0400

    add timer - commit 3

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.simpletimer;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -44,5 +50,26 @@ public class MainActivity extends AppCompatActivity {
             }
             //run log after 1000ms
         }, 1000);
+    }
+    public void doAlarm(View view) {
+        Intent intent = new Intent(this, MyService.class);
+
+        PendingIntent pendingIntent
+                = PendingIntent.getService(this, 0, intent, 0);
+
+        AlarmManager alarmManager
+                = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        alarmManager.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                System.currentTimeMillis(),
+                5000,
+                pendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP,
+                    System.currentTimeMillis() + 5000,
+                    pendingIntent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/simpletimer/MyService.java
@@ -0,0 +1,25 @@
+package com.example.simpletimer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private static final String TAG = "StartCommandTAG_";
+
+    public MyService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
